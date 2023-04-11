b22bd103c3e9ad8d9028f7bb9d271dd9b04f8173
Joby Elliott <code@byjoby.com>
2023-04-11 11:09:44 -0600

    bug fix

--- phinx/migrations/20230411170125_order_creation_dates.php
+++ phinx/migrations/20230411170125_order_creation_dates.php
@@ -9,10 +9,10 @@ final class OrderCreationDates extends AbstractMigration
     public function change(): void
     {
         $this->table('regalia_order')
-            ->addColumn('created', 'bigint', ['signed' => false, 'default' => time()])
+            ->addColumn('created', 'biginteger', ['signed' => false, 'default' => time()])
             ->save();
         $this->table('regalia_order')
-            ->changeColumn('created', 'bigint', ['signed' => false])
+            ->changeColumn('created', 'biginteger', ['signed' => false])
             ->save();
     }
 }
