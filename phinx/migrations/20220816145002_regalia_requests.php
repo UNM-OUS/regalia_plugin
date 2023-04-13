f4197256bf1216c2da88509cc57e24f29df41baa
Joby Elliott <code@byjoby.com>
2023-04-13 12:49:10 -0600

    phinx update fixes

--- phinx/migrations/20220816145002_regalia_requests.php
+++ phinx/migrations/20220816145002_regalia_requests.php
@@ -9,13 +9,13 @@ final class RegaliaRequests extends AbstractMigration
     public function change(): void
     {
         $this->table('regalia_request')
-            ->addColumn('semester', 'integer')
-            ->addColumn('identifier', 'string', ['length' => 150])
-            ->addColumn('parent', 'uuid')
+            ->addColumn('semester', 'integer', ['null' => false])
+            ->addColumn('identifier', 'string', ['length' => 150, 'null' => false])
+            ->addColumn('parent', 'uuid', ['null' => false])
             ->addColumn('preferred_group', 'string', ['length' => 50, 'null' => true])
-            ->addColumn('cancelled', 'boolean')
+            ->addColumn('cancelled', 'boolean', ['null' => false])
             ->addColumn('assigned_order', 'integer', ['null' => true])
-            ->addColumn('data', 'json')
+            ->addColumn('data', 'json', ['null' => false])
             ->addForeignKey('assigned_order', 'regalia_order')
             ->addIndex(['semester', 'identifier', 'parent'], ['unique' => true])
             ->addIndex('semester')
--- phinx/migrations/20230411170125_order_creation_dates.php
+++ phinx/migrations/20230411170125_order_creation_dates.php
@@ -9,10 +9,10 @@ final class OrderCreationDates extends AbstractMigration
     public function change(): void
     {
         $this->table('regalia_order')
-            ->addColumn('created', 'biginteger', ['signed' => false, 'default' => time()])
+            ->addColumn('created', 'biginteger', ['signed' => false, 'default' => time(), 'null' => false])
             ->save();
         $this->table('regalia_order')
-            ->changeColumn('created', 'biginteger', ['signed' => false])
+            ->changeColumn('created', 'biginteger', ['signed' => false, 'null' => false])
             ->save();
     }
 }
