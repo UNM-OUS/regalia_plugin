e4e087b9f0b19f0ffd01b533e0cf10203d1648c5
Joby Elliott <code@byjoby.com>
2023-04-14 10:46:47 -0600

    regalia migration cleanups for phinx 0.13

---
+++ phinx/migrations/20230414155538_phinx_regalia_cleanup.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+/**
+ * This migration is being added to coincide with updating to Phinx 0.13, to
+ * help make sure all the index columns line up with the new default types.
+ */
+final class PhinxRegaliaCleanup extends AbstractMigration
+{
+    public function change(): void
+    {
+        // drop all foreign keys to primary key id columns
+        $foreign_keys = [
+            'regalia_order' => ['column' => 'group_id', 'table' => 'regalia_group'],
+            'regalia_request' => ['column' => 'assigned_order', 'table' => 'regalia_order']
+        ];
+        foreach ($foreign_keys as $table => $key) {
+            $this->table($table)
+                ->dropForeignKey($key['column'])
+                ->save();
+        }
+        // update all the primary key id columns
+        $primary_keys = [
+            'regalia_billing', 'regalia_group', 'regalia_order', 'regalia_request'
+        ];
+        foreach ($primary_keys as $table) {
+            $this->table($table)
+                ->changeColumn('id', 'integer', ['signed' => false, 'null' => false, 'identity' => true])
+                ->changePrimaryKey('id')
+                ->save();
+        }
+        // re-add all the foreign keys that reference primary key id columns
+        foreach ($foreign_keys as $table => $key) {
+            $this->table($table)
+                ->changeColumn($key['column'], 'integer', ['null' => false, 'signed' => false])
+                ->addForeignKey($key['column'], $key['table'])
+                ->save();
+        }
+    }
+}
